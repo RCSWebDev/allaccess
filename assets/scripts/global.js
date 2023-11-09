61f979251efc6e5e1ef0b5e734b02716f27e0e78
May Chi <chi.mayling@gmail.com>
2023-11-09 11:22:43 -0500

    11/9/2023
    Updated homepage with Muzooka chart. Removed the Mediabase Charts from the homepage. Added Chartmetric and In-Store Music chart pages.

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -2,18 +2,28 @@ $(document).ready(function() {
 	var $browser = $(window).width();
 	
 	if($browser < 992) {
+		//* Navigation
 		$('.nav-primary').addClass('offcanvas offcanvas-start');
 		$('.nav-primary #navigation-primary').addClass('flex-column');
 		
+		//* Footer
 		$('.site-footer .container').addClass('text-center');
 		$('.site-footer .nav').addClass('justify-content-center');
+		
+		//* Homepage Box Ad
+		$('#side-box-ad').addClass('d-flex justify-content-center');
 	}
 	
 	if($browser >= 992) {
+		//* Navigation
 		$('.nav-primary').addClass('d-flex justify-content-end');
 		$('.nav-primary #navigation-primary').addClass('justify-content-end');
 		$('.nav-primary #navigation-social').addClass('justify-content-end');
 		
+		//* Footer
 		$('.site-footer .nav').addClass('justify-content-end');
+		
+		//* Homepage Box Ad
+		$('#side-box-ad img').addClass('float-end');
 	} 
 });
