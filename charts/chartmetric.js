a86fc2e65ce6622d6e80adf19af18735064b8e28
Corey Horob <coreyhorob@Coreys-MBP.verizon.net>
2023-12-14 13:05:24 -0500

    updated chart info

--- charts/chartmetric.js
+++ charts/chartmetric.js
@@ -1,60 +1,6 @@
 import { Grid, html } from "https://unpkg.com/gridjs?module";
+import chartData from "/charts/aa.json" assert { type: "json" };
 
-var tokenID;
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({
-  refreshtoken:
-    "SbfZLFnHhK2m2PWSzYgShdYhLHJOj9xJ0iJX2HvKoL44Ng95vggZA2NELGtcxrPG",
-});
-
-var requestOptions = {
-  method: "POST",
-  headers: myHeaders,
-  body: raw,
-  redirect: "follow",
-};
-
-function currenttoken() {
-  return fetch("https://api.chartmetric.com/api/token", requestOptions)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      return data.token;
-      //console.log(tokenID);
-    });
-}
-
-let newToken = await currenttoken();
-//console.log("Authorization", "Bearer " + newToken);
-
-var myHeaders1 = new Headers();
-myHeaders1.append("Authorization", "Bearer " + newToken);
-
-var requestOptions1 = {
-  method: "GET",
-  headers: myHeaders1,
-  redirect: "follow",
-};
-
-function getChart() {
-  return fetch(
-    "https://api.chartmetric.com/api/artist/list/filter?limit=100&sortOrderDesc=false",
-    requestOptions1
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      return data;
-      //console.log(tokenID);
-    });
-}
-
-//console.log("test");
-let chartData = await getChart();
 //console.log(chartData);
 const data = chartData.obj.obj.map((artist) => ({
   cm_artist_rank: artist.cm_artist_rank,
